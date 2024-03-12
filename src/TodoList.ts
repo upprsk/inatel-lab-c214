fb5531566a63696bbc5b3f3323e74126e52ced29
upprsk <lucassilveiraross@gmail.com>
2024-03-12 15:56:11 -0300

    feat: :sparkles: validate task before add

--- src/TodoList.ts
+++ src/TodoList.ts
@@ -11,7 +11,20 @@ export class TodoList {
   private tasks: Task[] = [];
 
   add(task: Task) {
-    this.tasks.push(task);
+    // peak typescript good practices
+    const missingProperties = ['title', 'description', 'targetDate'].filter(
+      prop => !Object.keys(task).includes(prop),
+    );
+
+    try {
+      if (missingProperties.length > 0) {
+        return `Missing properties in object: ${missingProperties}`;
+      }
+
+      this.tasks.push(task);
+    } catch (error) {
+      return error;
+    }
   }
 
   getTasks() {
